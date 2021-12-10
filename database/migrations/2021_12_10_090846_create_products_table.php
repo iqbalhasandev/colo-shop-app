36bfd13e23cde7cf99c6ca4a86c990a85271d071
IQBAL HASAN <iqbalhasan.dev@gmail.com>
2021-12-10 15:33:56 +0600

    Product Module added

---
+++ database/migrations/2021_12_10_090846_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('shot_dec');
+            $table->text('long_dec');
+            $table->integer('current_price');
+            $table->integer('descount_price');
+            $table->unsignedBigInteger('category_id');
+            $table->string('img1');
+            $table->string('img2');
+            $table->string('img3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
