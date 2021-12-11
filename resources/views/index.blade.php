5f92ff565c4a46d9375291a4b0a98960f2130a59
IQBAL HASAN <iqbalhasan.dev@gmail.com>
2021-12-11 16:23:39 +0600

    Single product view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,247 +1,116 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-app-layout>
+    <!-- Slider -->
 
-<head>
-    <title>Colo Shop</title>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="Colo Shop Template">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
-    <link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
-    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
-    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
-    <link rel="stylesheet" type="text/css" href="styles/main_styles.css">
-    <link rel="stylesheet" type="text/css" href="styles/responsive.css">
-</head>
-
-<body>
-
-    <div class="super_container">
-
-        <!-- Header -->
-
-        <header class="header trans_300">
-            <!-- Top Navigation -->
-
-            <div class="top_nav">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="top_nav_left">
-                                free shipping on all u.s orders over $50
-                            </div>
-                        </div>
-                        <div class="col-md-6 text-right">
-                            <div class="top_nav_right">
-                                <ul class="top_nav_menu">
-                                    <li class="account">
-                                        <a href="#">
-                                            My Account
-                                            <i class="fa fa-angle-down"></i>
-                                        </a>
-                                        <ul class="account_selection">
-                                            <li>
-                                                <a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a>
-                                            </li>
-                                            <li>
-                                                <a href="#"><i class="fa fa-user-plus"
-                                                        aria-hidden="true"></i>Register</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Main Navigation -->
-
-            <div class="main_nav_container">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12 text-right">
-                            <div class="logo_container">
-                                <a href="index.html">colo<span>shop</span></a>
-                            </div>
-                            <nav class="navbar">
-                                <ul class="navbar_menu">
-                                    <li><a href="index.html">home</a></li>
-                                    <li><a href="contact.html">contact</a></li>
-                                </ul>
-                                <ul class="navbar_user">
-                                    <li class="checkout">
-                                        <a href="#">
-                                            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-                                            <span id="checkout_items" class="checkout_items">2</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <div class="fs_menu_overlay"></div>
-
-        <!-- Slider -->
-
-        <div class="main_slider" style="background-image:url({{ Voyager::image(setting('site.banner_image')) }})">
-            <div class="container fill_height">
-                <div class="row align-items-center fill_height">
-                    <div class="col">
-                        <div class="main_slider_content">
-                            <h6>{{ setting('site.banner_subtitle') }}</h6>
-                            <h1>{{ setting('site.banner_title') }}</h1>
-                            <div class="red_button shop_now_button"><a href="#">shop now</a></div>
-                        </div>
+    <div class="main_slider" style="background-image:url({{ Voyager::image(setting('site.banner_image')) }})">
+        <div class="container fill_height">
+            <div class="row align-items-center fill_height">
+                <div class="col">
+                    <div class="main_slider_content">
+                        <h6>{{ setting('site.banner_subtitle') }}</h6>
+                        <h1>{{ setting('site.banner_title') }}</h1>
+                        <div class="red_button shop_now_button"><a href="#shopping">shop now</a></div>
                     </div>
                 </div>
             </div>
         </div>
-
-
-
-        <!-- New Arrivals -->
-        <div class="new_arrivals">
-            <div class="container">
-                <div class="row">
-                    <div class="col text-center">
-                        <div class="section_title new_arrivals_title">
-                            <h2>New Arrivals</h2>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <div class="product-grid"
-                            data-isotope='{ "itemSelector": ".product-item", "layoutMode": "fitRows" }'>
-
-                            <!-- Product 1 -->
-                            @foreach ($products as $product)
-                            <div class="product-item men">
-                                <div class="product discount product_filter">
-                                    <div class="product_image">
-                                        <img src="{{ asset('storage/'.$product->img1) }}" alt="">
-                                    </div>
-                                    <div class="favorite favorite_left"></div>
-
-                                    <div class="product_info">
-                                        <h6 class="product_name">
-                                            <a href="single.html">{{ $product->name }}</a>
-                                        </h6>
-                                        <div class="product_price">${{ $product->descount_price }}<span>${{
-                                                $product->current_price }}</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="red_button add_to_cart_button"><a href="#">add to cart</a></div>
-                            </div>
-                            @endforeach
-
-                            <!-- Product 2 -->
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Benefit -->
-
-        <div class="benefit">
-            <div class="container">
-                <div class="row benefit_row">
-                    <div class="col-lg-3 benefit_col">
-                        <div class="benefit_item d-flex flex-row align-items-center">
-                            <div class="benefit_icon"><i class="fa fa-truck" aria-hidden="true"></i></div>
-                            <div class="benefit_content">
-                                <h6>free shipping</h6>
-                                <p>Suffered Alteration in Some Form</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 benefit_col">
-                        <div class="benefit_item d-flex flex-row align-items-center">
-                            <div class="benefit_icon"><i class="fa fa-money" aria-hidden="true"></i></div>
-                            <div class="benefit_content">
-                                <h6>cach on delivery</h6>
-                                <p>The Internet Tend To Repeat</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 benefit_col">
-                        <div class="benefit_item d-flex flex-row align-items-center">
-                            <div class="benefit_icon"><i class="fa fa-undo" aria-hidden="true"></i></div>
-                            <div class="benefit_content">
-                                <h6>45 days return</h6>
-                                <p>Making it Look Like Readable</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 benefit_col">
-                        <div class="benefit_item d-flex flex-row align-items-center">
-                            <div class="benefit_icon"><i class="fa fa-clock-o" aria-hidden="true"></i></div>
-                            <div class="benefit_content">
-                                <h6>opening all week</h6>
-                                <p>8AM - 09PM</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Footer -->
-
-        <footer class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div
-                            class="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
-                            <ul class="footer_nav">
-                                <li><a href="contact.html">Contact us</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div
-                            class="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
-                            <ul>
-                                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="footer_nav_container">
-                            <div class="cr">©2021 All Rights Reserverd. Made with <i class="fa fa-heart-o"
-                                    aria-hidden="true"></i> by <a href="#">Computer Tecnology</a></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-
     </div>
 
-    <script src="js/jquery-3.2.1.min.js"></script>
-    <script src="styles/bootstrap4/popper.js"></script>
-    <script src="styles/bootstrap4/bootstrap.min.js"></script>
-    <script src="plugins/Isotope/isotope.pkgd.min.js"></script>
-    <script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
-    <script src="plugins/easing/easing.js"></script>
-    <script src="js/custom.js"></script>
-</body>
 
-</html>
+    <!-- New Arrivals -->
+    <div class="new_arrivals" id="shopping" style="margin-top:40px ">>
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    <div class="section_title new_arrivals_title">
+                        <h2>New Arrivals</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="product-grid"
+                        data-isotope='{ "itemSelector": ".product-item", "layoutMode": "fitRows" }'>
+
+                        <!-- Product 1 -->
+                        @foreach ($products as $product)
+                        <div class="product-item men">
+                            <div class="product discount product_filter">
+                                <div class="product_image">
+                                    <img src="{{ asset('storage/'.$product->img1) }}" alt="">
+                                </div>
+                                <div class="favorite favorite_left"></div>
+
+                                <div class="product_info">
+                                    <h6 class="product_name">
+                                        <a href="{{ route('product.show',$product->id) }}">{{ $product->name }}</a>
+                                    </h6>
+                                    <div class="product_price">${{ $product->descount_price }}<span>${{
+                                            $product->current_price }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="red_button add_to_cart_button"><a href="#">add to cart</a></div>
+                        </div>
+                        @endforeach
+
+                        <!-- Product 2 -->
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Benefit -->
+
+    <div class="benefit">
+        <div class="container">
+            <div class="row benefit_row">
+                <div class="col-lg-3 benefit_col">
+                    <div class="benefit_item d-flex flex-row align-items-center">
+                        <div class="benefit_icon"><i class="fa fa-truck" aria-hidden="true"></i></div>
+                        <div class="benefit_content">
+                            <h6>free shipping</h6>
+                            <p>Suffered Alteration in Some Form</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 benefit_col">
+                    <div class="benefit_item d-flex flex-row align-items-center">
+                        <div class="benefit_icon"><i class="fa fa-money" aria-hidden="true"></i></div>
+                        <div class="benefit_content">
+                            <h6>cach on delivery</h6>
+                            <p>The Internet Tend To Repeat</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 benefit_col">
+                    <div class="benefit_item d-flex flex-row align-items-center">
+                        <div class="benefit_icon"><i class="fa fa-undo" aria-hidden="true"></i></div>
+                        <div class="benefit_content">
+                            <h6>45 days return</h6>
+                            <p>Making it Look Like Readable</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 benefit_col">
+                    <div class="benefit_item d-flex flex-row align-items-center">
+                        <div class="benefit_icon"><i class="fa fa-clock-o" aria-hidden="true"></i></div>
+                        <div class="benefit_content">
+                            <h6>opening all week</h6>
+                            <p>8AM - 09PM</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    @push('extra-style')
+    <link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+    @endpush
+    @push('extra-script')
+    <script src="js/custom.js"></script>
+    @endpush
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProductController;
+use TCG\Voyager\Facades\Voyager;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ProductController::class, 'index']);
-
-
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
+});
+
+
+Route::prefix('/')->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('product.index');
+    Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+    Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+    Route::get('/{product}', [ProductController::class, 'show'])->name('product.show');
 });
